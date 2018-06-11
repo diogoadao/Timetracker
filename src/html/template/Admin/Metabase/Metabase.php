5ff65408adce2a0df26a830e86527d7cef7cca1b
Diogo <diogoadao@miguelcaldas.com>
2018-06-11 16:19:05 +0100

    fixed metabase link

--- src/html/template/Admin/Metabase/Metabase.php
+++ src/html/template/Admin/Metabase/Metabase.php
@@ -1,4 +1,4 @@
 <?php 
-echo'<iframe id="metabase" src="https://metabase-visualthinking.herokuapp.com/public/dashboard/fc89f093-5b64-4050-91b9-3350a2110a4f" frameborder="0" width="100%"
+echo'<iframe id="metabase" src="https://thmc.ddns.net/powerbiembed/Home/EmbedReport" frameborder="0" width="100%"
     height="67%" allowtransparency></iframe>';
 ?>
